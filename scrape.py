0a754e8f9e13fb81331e6581295ba4b695f1e79c
tacombel <tacombel@gmail.com>
2020-04-17 13:34:01 +0200

    Bug: no se ejecutaba el c.execute para la ultima fecha, solo para la anterior

--- scrape.py
+++ scrape.py
@@ -42,8 +42,7 @@ def look_for_data():
             else:
                 if date_old:
                     c.execute("INSERT OR REPLACE INTO cotizacion (fecha, VL, activo_id) VALUES (?, ?, ?)", (date_old, VL_old, e[0],))
-                else:
-                    c.execute("INSERT OR REPLACE INTO cotizacion (fecha, VL, activo_id) VALUES (?, ?, ?)", (date, VL, e[0],))
+                c.execute("INSERT OR REPLACE INTO cotizacion (fecha, VL, activo_id) VALUES (?, ?, ?)", (date, VL, e[0],))
 
             remove.append(index)
         conn.commit()

fb918eeb75906457e4de4802adba82b0b7ffb210
tacombel <tacombel@gmail.com>
2020-05-25 17:35:56 +0200

    evaluo los resultados a medida que van acabando

--- scrape.py
+++ scrape.py
@@ -18,43 +18,47 @@ def look_for_data():
         candidates.append(row[0])
 
     n = 1
-    while True:
-        hora_de_inicio = time.time()
-        if len(candidates) == 1:
-            print('Scrapeando ', len(candidates), 'valor. Intento número', n)
-        else:
-            print('Scrapeando ', len(candidates), 'valores. Intento número', n)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            futures = executor.map(scrape, candidates)
-        for future in futures:
-            print(future)
-            if len(future) == 6:
-                c.execute("INSERT OR REPLACE INTO cotizacion (fecha, VL, activo_id) VALUES (?, ?, ?)",
-                          (future[2], future[3], future[-1],))
-                conn.commit()
-            if len(future) >= 4:
-                c.execute("INSERT OR REPLACE INTO cotizacion (fecha, VL, activo_id) VALUES (?, ?, ?)",
-                          (future[0], future[1], future[-1],))
-                conn.commit()
-                candidates.remove(future[-1])
-            if len(future) == 3 and future[0] == 'Error':
-                candidates.remove(future[-1])
-        duracion = (time.time() - hora_de_inicio) / 60
-        if duracion < 1:
-            duracion = duracion * 60
-            print('Duración de la descarga: ', '{:.2f}'.format(duracion), 'segundos', flush=True)
-        else:
-            print('Duración de la descarga: ', '{:.2f}'.format(duracion), 'minutos', flush=True)
 
-        if len(candidates) == 0:
-            break
-        n += 1
-        if n < 4:
-            print('---Los siguientes valores han dado error y se volvera a intentar: ', candidates)
-        else:
-            print('Demasiados reintentos. Abortando')
-            break
-        time.sleep(30)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        while True:
+            hora_de_inicio = time.time()
+            if len(candidates) == 1:
+                print('Scrapeando ', len(candidates), 'valor. Intento número', n)
+            else:
+                print('Scrapeando ', len(candidates), 'valores. Intento número', n)
+            wait_for = []
+            for candidate in candidates:
+                wait_for.append(executor.submit(scrape, candidate))
+            for future in concurrent.futures.as_completed(wait_for):
+                future = future.result()
+                print(future)
+                if len(future) == 6:
+                    c.execute("INSERT OR REPLACE INTO cotizacion (fecha, VL, activo_id) VALUES (?, ?, ?)",
+                              (future[2], future[3], future[-1],))
+                    conn.commit()
+                if len(future) >= 4:
+                    c.execute("INSERT OR REPLACE INTO cotizacion (fecha, VL, activo_id) VALUES (?, ?, ?)",
+                              (future[0], future[1], future[-1],))
+                    conn.commit()
+                    candidates.remove(future[-1])
+                if len(future) == 3 and future[0] == 'Error':
+                    candidates.remove(future[-1])
+            duracion = (time.time() - hora_de_inicio) / 60
+            if duracion < 1:
+                duracion = duracion * 60
+                print('Duración de la descarga: ', '{:.2f}'.format(duracion), 'segundos', flush=True)
+            else:
+                print('Duración de la descarga: ', '{:.2f}'.format(duracion), 'minutos', flush=True)
+
+            if len(candidates) == 0:
+                break
+            n += 1
+            if n < 4:
+                print('---Los siguientes valores han dado error y se volvera a intentar: ', candidates)
+            else:
+                print('Demasiados reintentos. Abortando')
+                break
+            time.sleep(30)
 
     current_time = time.time()
     c.execute("INSERT OR REPLACE INTO variables (name, value) VALUES (?,?)", ("last_scrape", current_time))

1fd45e53fca7f56e3d4d71761165ff75cc37c160
tacombel <tacombel@gmail.com>
2020-05-30 15:23:06 +0200

    print de control

--- scrape_one.py
+++ scrape_one.py
@@ -15,6 +15,7 @@ import logging
 
 
 def descargar_pagina(url):
+    print('Acraping ', url)
     cwd = os.getcwd()
     # chromedriver para intel y chromedriver_ARM para raspberry
     if platform.system() == 'Windows':

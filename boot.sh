d4da0f1586fad771d8b59113e1056b684bc04a06
Daniel <tacombel@gmail.com>
2022-05-20 19:38:33 +0200

    Para que corra bajo nginx

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-exec gunicorn -b :5000 --timeout 240 --access-logfile - --error-logfile - portfolio3:app
+exec gunicorn -b :5100 --timeout 240 --access-logfile - --error-logfile - portfolio3:app
